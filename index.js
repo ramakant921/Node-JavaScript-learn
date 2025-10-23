a983054db5a0551bce91b319fe52d2dc85b0ecd0
Ramakant MIshra <ramakantm921@gmail.com>
2025-10-23 19:04:43 +0530

    github data commit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 6060
+githubData = {
+    "name": "Spodormon",
+    "company": "spodormon.com",
+    "location": "Earth",
+    "repositories": 50,
+    "followers": 1000
+}
 
 app.get('/', (req, res) => {
   res.send('Spodormon is saying Hello World!')
@@ -17,6 +24,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send('<h2>Subscribe to Spodormon channel on YouTube nigaaassssssss..</h2>')
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
